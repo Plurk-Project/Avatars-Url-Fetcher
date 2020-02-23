b365e91640ba26558ebb63f656bb2a70ac11ee25
SheiUn <me.sheiun@gmail.com>
2020-02-24 02:49:46 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const plurks = fs.readFileSync("plurks.txt", "utf-8").split("\n");
+console.log(plurks[0]);
+
+// async function asyncForEach(array, callback) {
+//   for (let index = 0; index < array.length; index++) {
+//     await callback(array[index], index, array);
+//   }
+// }
+
+// (async () => {
+//   asyncForEach(plurks, async plurk => {
+//     const html = await fetch(plurk).then(res => res.text());
+//     const $ = cheerio.load(html);
+//     const avatar = $("#profile_pic").attr("src");
+
+//     fs.appendFileSync("data.csv", `${plurk},${avatar}\n`);
+//     console.log(`${plurk} Done!`);
+//   });
+// })();
