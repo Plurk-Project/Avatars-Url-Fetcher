777a822505c270f9121b621b61e368aa3d12300c
SheiUn <me.sheiun@gmail.com>
2020-02-24 03:02:15 +0800

    feat: update with arg parser

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const ArgumentParser = require("argparse").ArgumentParser;
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const plurks = fs.readFileSync("plurks.txt", "utf-8").split("\n");
+const parser = new ArgumentParser({
+  addHelp: true
+});
+parser.addArgument(["-i", "--input"], {
+  required: true,
+  help: "Input text path. (seperate user url with new line)"
+});
+parser.addArgument(["-o", "--output"], {
+  required: true,
+  help: "Output csv format path."
+});
+const args = parser.parseArgs();
+
+const plurks = fs.readFileSync(args.input, "utf-8").split("\n");
 
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
@@ -16,7 +30,7 @@ async function asyncForEach(array, callback) {
     const $ = cheerio.load(html);
     const avatar = $("#profile_pic").attr("src");
 
-    fs.appendFileSync("data.csv", `${plurk},${avatar}\n`);
+    fs.appendFileSync(args.output, `${plurk},${avatar}\n`);
     console.log(`${plurk} Done!`);
   });
 })();
